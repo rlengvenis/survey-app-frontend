191dfbdc17248bd923d4090037a3419aaeee45ce
Rokas Lengvenis <rokas.lengvenis@devbridge.com>
2018-01-09 14:32:08 +0700

    Add mobile navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,56 +2,87 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 
-export const Header = (props) => {
-  const {
-    surveyId,
-    authenticated
-  } = props;
+const MEDIA_MD = 600; // The viewport min size, where mobile navigation bar is not shown any more
 
-  return (
-    <header className="app__header">
-      <nav>
-        <ul className="navigation__list">
-          <li className="navigation__list-item">
-            <NavLink
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-              to="/builder"
-            >
-              Survey Builder
-            </NavLink>
-          </li>
-          <li className="navigation__list-item">
-            <NavLink
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-              to={{
-                pathname: '/survey',
-                search: surveyId && `id=${surveyId}`
-              }}
-            >
-              Survey
-            </NavLink>
-          </li>
-          <li className="navigation__list-item">
-            <NavLink
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-              to="/responses"
-            >
-              Responses
-            </NavLink>
-          </li>
+export class Header extends React.Component {
 
-          {renderAuthLinks(authenticated)}
+  constructor(props) {
+    super(props);
 
-        </ul>
-      </nav>
-    </header>
-  );
-};
+    this.state = {
+      isNavListVisible: window.innerWidth >= MEDIA_MD
+    };
+  }
+
+  render() {
+    const {
+      surveyId,
+      authenticated
+    } = this.props;
+
+
+    const navigationStyle = classnames('navigation__list', {
+      'navigation__list--hidden': !this.state.isNavListVisible
+    });
+
+    return (
+      <header className="app__header">
+        <nav>
+          <i
+            className="material-icons navigation__menu"
+            onClick={this.handleNavigationVisibilityToggle}
+          >
+            menu
+          </i>
+          <ul className={navigationStyle}>
+            <li className="navigation__list-item">
+              <NavLink
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+                to="/builder"
+              >
+                Survey Builder
+              </NavLink>
+            </li>
+            <li className="navigation__list-item">
+              <NavLink
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+                to={{
+                  pathname: '/survey',
+                  search: surveyId && `id=${surveyId}`
+                }}
+              >
+                Survey
+              </NavLink>
+            </li>
+            <li className="navigation__list-item">
+              <NavLink
+                className="navigation__link"
+                activeClassName="navigation__link--active"
+                to="/responses"
+              >
+                Responses
+              </NavLink>
+            </li>
+
+            {renderAuthLinks(authenticated)}
+
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+  handleNavigationVisibilityToggle = () => {
+    this.setState({
+      isNavListVisible: !this.state.isNavListVisible
+    });
+  }
+}
 
 Header.propTypes = {
   authenticated: PropTypes.bool,
